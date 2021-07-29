6111f6e600bf86276e149e51251104e736a11111
Benjamin Calderon <benjamin.calderon@carvana.com>
2021-07-29 10:02:32 -0700

    added file saving

--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require("fs")
+
 const app = express()
 
 app.use(express.json())
@@ -9,6 +11,8 @@ var leaderboard = [
     {name: "Bradley", score: 130},
 ]
 
+fs.writeFileSync("leaderboard.json", JSON.stringify(leaderboard))
+
 app.get("/", (req, res) => {
     leaderboard.sort((a, b) => (a.score < b.score) ? 1 : -1)
     res.send(JSON.stringify(leaderboard))
@@ -17,6 +21,7 @@ app.get("/", (req, res) => {
 app.put("/", (req, res) => {
     leaderboard.push(req.body)
     leaderboard.sort((a, b) => (a.score < b.score) ? 1 : -1)
+    fs.writeFileSync("leaderboard.json", JSON.stringify(leaderboard))
     res.send(JSON.stringify(leaderboard))
 })
 
