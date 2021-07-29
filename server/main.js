168e7cd0b85ce3370b0358e28ca7dac2c3d880c9
Benjamin Calderon <benjamin.calderon@carvana.com>
2021-07-29 13:35:06 -0700

    add shell and file reading for server

--- server/main.js
+++ server/main.js
@@ -1,24 +1,21 @@
 const express = require('express')
 const fs = require("fs")
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-var leaderboard = [
-    {name: "Ben", score: 100},
-    {name: "Bradley", score: 130},
-]
-
-fs.writeFileSync("leaderboard.json", JSON.stringify(leaderboard))
-
 app.get("/", (req, res) => {
-    leaderboard.sort((a, b) => (a.score < b.score) ? 1 : -1)
+    var leaderboard = JSON.parse(fs.readFileSync("leaderboard.json").toString())
     res.send(JSON.stringify(leaderboard))
 })
 
 app.get("/add", (req, res) => {
+    var leaderboard = JSON.parse(fs.readFileSync("leaderboard.json").toString())
     leaderboard.push({
         name: req.query.name,
         score: req.query.score
