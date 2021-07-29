8de01dacfd38521032abf91e8a4fe2d70edf2bf9
Benjamin Calderon <benjamin.calderon@carvana.com>
2021-07-29 11:25:25 -0700

    leaderboard

---
+++ scenes/Leaderboard.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$HTTPRequest.request("http://138.197.78.108:5000")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	if len(json.result) < 10:
+		for i in len(json.result):
+			$scores.text += "%s %s\n" % [json.result[i].name, json.result[i].score]
+	else:
+		for i in range(10):
+			$scores.text += "%s %s\n" % [json.result[i].name, json.result[i].score]
