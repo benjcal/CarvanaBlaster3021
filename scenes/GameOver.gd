9f27bd543ce3c2bb8926b8e4211f7c4b6682c00f
Benjamin Calderon <benjamin.calderon@carvana.com>
2021-07-29 16:58:23 -0700

    leaderboard working

--- scenes/GameOver.gd
+++ scenes/GameOver.gd
@@ -6,8 +6,12 @@ export var level = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Score.text = "Score %s Level %s" % [score, level]
+	$BGMusic.play()
 
 func _on_LineEdit_text_entered(new_text):
+	$Select.play()
 	$HTTPRequest.request("http://138.197.78.108:5000/add?name=%s&score=%s&level=%s" % [$LineEdit.text, score, level])
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	get_tree().change_scene("res://scenes/MainMenu.tscn")
-	
