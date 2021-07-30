a611bf4e2fd974867677d47324bcc01f94049216
Benjamin Calderon <benjamin.calderon@carvana.com>
2021-07-30 09:01:06 -0700

    server changes

--- server/main.js
+++ server/main.js
@@ -10,20 +10,20 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-    var leaderboard = JSON.parse(fs.readFileSync("leaderboard.json").toString())
+    var leaderboard = JSON.parse(fs.readFileSync("/root/leaderboard.json").toString())
     leaderboard.sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1)
     res.send(JSON.stringify(leaderboard))
 })
 
 app.get("/add", (req, res) => {
-    var leaderboard = JSON.parse(fs.readFileSync("leaderboard.json").toString())
+    var leaderboard = JSON.parse(fs.readFileSync("/root/leaderboard.json").toString())
     leaderboard.push({
         name: req.query.name,
         score: req.query.score,
         level: req.query.level
     })
     leaderboard.sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1)
-    fs.writeFileSync("leaderboard.json", JSON.stringify(leaderboard))
+    fs.writeFileSync("~/leaderboard.json", JSON.stringify(leaderboard))
     res.send(JSON.stringify(leaderboard))
 })
 
